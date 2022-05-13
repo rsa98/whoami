08f5c6f8af09ebe1a30e66df59afcd12d603823c
Christian Henrique <christianhmartins@gmail.com>
2022-05-12 22:59:33 -0300

    Todos arquivos de estilo

--- whoami/src/App.tsx
+++ whoami/src/App.tsx
@@ -1,25 +1,18 @@
+// Bibliotecas
 import { useState } from 'react'
+import router from 'react-router';
+// Estilos
 import { Global } from './styles/global';
 import './styles/reset.css';
+// Assets
+// Outros
 
 function App() {
 
   return (
-    <Global>
-      <h1>H1</h1>
-      <h2>H2</h2>
-      <h3>H3</h3>
-      <h4>H4</h4>
-      <h5>H5</h5>
-      <h6>H6</h6>
-      <p className="paragraph-xl">Paragrafo large</p>
-      <p className="paragraph-lg">Paragrafo large</p>
-      <p className="paragraph-md">Paragrafo medium</p>
-      <p className="paragraph-sm">Paragrafo small</p>
-      <p className="paragraph-xs">Paragrafo small</p>
-      <p className="paragraph-quack">Paragrafo small</p>
-      <p className="paragraph-nano">Paragrafo small</p>
-    </Global>
+    <>
+    <h1>hello world</h1>
+    </>
   )
 }
 
